49f10080c27d4478647ce116f3f64c02eb907cad
Ahsan Waseem <wahsan77@yahoo.com>
2023-10-14 11:42:51 -0700

    spelling error

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <p>Professionally, I am a Product Manager at Microsoft.</p>
         <p>I graduated from Texas A&M University, <i>ivy of the south they say</i>, in May of 2023 with a Bachelor of Science in Multidiscipliary Engineering with a concentration in Computer Science and Business.</p>
         <p>
-          I started to code after reading the Almanak of Naval Ravikant and learning about permissionless leverage. A month after reading that book I created a website that reached 150k+ users in 70+ countries. Now, I seek that leverage he talks about.
+          I started to code after reading the Almanack of Naval Ravikant and learning about permissionless leverage. A month after reading that book I created a website that reached 150k+ users in 70+ countries. Now, I seek that leverage he talks about.
         </p>
         <p>
          When I'm exhausted being him, I'm just Ahsan, a silly goofy little (6 foot 2) dude!
