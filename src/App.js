cc4153ed5ef134cdee7c567fcd50ade01e72e905
Ahsan Waseem <ahsanwaseem1120@gmail.com>
2025-07-01 12:00:09 -0700

    reword

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <p>I'm a Product Manager that loves to code; currently working at Microsoft!</p>
         <p>I graduated from Texas A&M University in 2023 with a concentration in Computer Science.</p>
         <p>
-        Pre-college, I explored various mediums of creation like music production, podcasts, and video editing. After reading the Almanack of Naval Ravikant my freshamn year of college and learning about his philosophy of permissionless leverage, I learned to code. A month after reading that book, I created a website that reached over 200k users; now I seek the leverage he talks about.
+        Pre-college, I explored various mediums of creation like music production, podcasts, and video editing. After reading the Almanack of Naval Ravikant and learning about his philosophy of permissionless leverage, I was inspired to code. A month after reading that book, I created a website that reached over 200k users; now I seek the leverage he talks about.
         </p>
     
       </section>
